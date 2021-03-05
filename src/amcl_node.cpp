24c714d3e370fbbead5bd377b22cbf1b2f42b6db
zoltan-lengyel <79632988+zoltan-lengyel@users.noreply.github.com>
2021-03-05 13:06:51 +0100

    Fixed uninitialized frame names becoming valid after the tf_prefix is removed
    (cherry picked from commit d820b8fba6de8072f0f78805e62d94a73bcd9dd2)

--- src/amcl_node.cpp
+++ src/amcl_node.cpp
@@ -438,9 +438,9 @@ AmclNode::AmclNode() :
 
   private_nh_.param("update_min_d", d_thresh_, 0.2);
   private_nh_.param("update_min_a", a_thresh_, M_PI/6.0);
-  private_nh_.param("odom_frame_id", odom_frame_id_, std::string("odom"));
-  private_nh_.param("base_frame_id", base_frame_id_, std::string("base_link"));
-  private_nh_.param("global_frame_id", global_frame_id_, std::string("map"));
+  private_nh_.param("odom_frame_id", odom_frame_id_, std::string("uninitialized_odom"));
+  private_nh_.param("base_frame_id", base_frame_id_, std::string("uninitialized_base_link"));
+  private_nh_.param("global_frame_id", global_frame_id_, std::string("uninitialized_map"));
   private_nh_.param("resample_interval", resample_interval_, 2);
   private_nh_.param("selective_resampling", selective_resampling_, false);
   double tmp_tol;
